e10d021583696ab5dbe12df6727902767047e39a
Nathan Cordrey <lVathan@rocketmail.com>
2014-06-28 06:37:20 -0400

    initial flasky upload

---
+++ app/main/views.py
@@ -0,0 +1,29 @@
+from flask import render_template, session, redirect, url_for, current_app, flash
+from .. import db
+from ..models import User
+from ..email import send_email
+from . import main
+from .forms import NameForm
+from datetime import datetime
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user= User(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+            if current_app.config['FLASKY_ADMIN']:
+                send_email('nathan.cordrey@gmail.com', 'New User', 'mail/new_user', user=user)
+                flash('email sent!' + current_app.config['MAIL_USERNAME'])
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data=''
+        return redirect(url_for('.index'))
+    return render_template('index.html', form=form, name=session.get('name'), known = session.get('known', False),
+            current_time = datetime.utcnow())
+
+
